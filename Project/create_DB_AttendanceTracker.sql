04b23061780d40f815dea57d19ebe5e31a51d3d3
itadder <justinogarcia@justinogarciaconsulting.com>
2016-03-23 23:50:52 -0400

    Update create_DB_AttendanceTracker.sql

--- Project/create_DB_AttendanceTracker.sql
+++ Project/create_DB_AttendanceTracker.sql
@@ -1,28 +1,8 @@
 Drop database if exists AttendanceTracker;
 create database AttendanceTracker;
 use  AttendanceTracker;
+drop table if exists 'login';
 drop table if exists `employees`;
 
-create table `employees` (
-	employeeID int(10) primary key not null auto_increment,
-	fname varchar(255),
-	lname varchar(255),
-	role enum('Employee','Manager','HR'),
-	leaveTaken_dates varchar(10),
-	leavetaken_type enum('personal','vacation','sick')
-);
 
-insert into `employees` (
-	fname,
-	lname,
-	role,
-	leaveTaken_dates,
-	leavetaken_type)
-values (
-	"Bob",
-	"smith",
-	"Employee",
-	"12/20/2015",
-	"personal"
-);
 
